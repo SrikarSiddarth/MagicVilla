00bb29f0059b9135fb5dd5adf989743898d0f0cf
SrikarSiddarth <srikarsiddharth@gmail.com>
2023-07-26 22:10:42 +0530

    first commit

---
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,27 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VillaAPIController : ControllerBase    
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            return Ok(VillaStore.villaList.FirstOrDefault(u=>u.Id==id));
+        }
+    }
+}
